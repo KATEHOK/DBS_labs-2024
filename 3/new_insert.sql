bafea1ed90f6a4d6eb70ad5fb5cf4119a637b46d
Katehok <a.timin2003@mail.ru>
2024-06-01 19:06:53 +0300

    Done alter alter 3

---
+++ 3/new_insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO Actors (Name, DateOfBirth, Biography)
+VALUES
+    ('Emilia Clarke', '1986-10-23', 'Emilia Isobel Euphemia Rose Clarke is an English actress.'),
+    ('Matthew Perry', '1969-08-19', 'Matthew Langford Perry is a Canadian-American actor.'),
+    ('Aaron Paul', '1979-08-27', 'Aaron Paul Sturtevant is an American actor.'),
+    ('Sophie Turner', '1996-02-21', 'Sophie Belinda Turner is an English actress.'),
+    ('Courteney Cox', '1964-06-15', 'Courteney Bass Cox is an American actress.'),
+    ('Dean Norris', '1963-04-08', 'Dean Joseph Norris is an American actor.'),
+    ('Maisie Williams', '1997-04-15', 'Margaret Constance Williams is an English actress.'),
+    ('Lisa Kudrow', '1963-07-30', 'Lisa Valerie Kudrow is an American actress.'),
+    ('Giancarlo Esposito', '1958-04-26', 'Giancarlo Giuseppe Alessandro Esposito is an American actor.'),
+    ('Peter Dinklage', '1969-06-11', 'Peter Hayden Dinklage is an American actor.');
+
+INSERT INTO Cast (ActorID, ShowID, Role)
+VALUES
+    (4, 1, 'Daenerys Targaryen'),
+    (5, 2, 'Chandler Bing'),
+    (6, 3, 'Hank Schrader'),
+    (7, 1, 'Arya Stark'),
+    (8, 2, 'Monica Geller'),
+    (9, 3, 'Gustavo "Gus" Fring'),
+    (10, 1, 'Tyrion Lannister'),
+    (11, 2, 'Phoebe Buffay'),
+    (12, 3, 'Mike Ehrmantraut'),
+    (13, 1, 'Cersei Lannister');
